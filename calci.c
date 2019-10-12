b5d9d55847e14978c4b68261c253692de8ce4958
venkatkeshav <52830976+venkatkeshav@users.noreply.github.com>
2019-10-12 17:08:06 +0530

    Update calci.c

--- calci.c
+++ calci.c
@@ -11,6 +11,11 @@ int main()
     if(c==1)printf("Sum is:%d\n",a+b);
     if(c==2)printf("Diff is:%d",a-b);
     if(c==3)printf("Product is:%d",a*b);
-    if(c==4)printf("Quotient is%d",a/b);
+    if(c==4)
+    {  if(b!=0)
+        printf("Quotient is:%d",a/b);
+     else 
+         printf("division is not possible as b=0");
+                }
     return 0;
 }
